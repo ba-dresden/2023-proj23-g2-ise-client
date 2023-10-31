2521c6f4d9863f0e775d94fb1cd21c0d6617d7f1
Dennik Klemt <klemtdennik@gmail.com>
2023-10-31 09:32:07 +0100

    SpeedControl updated

--- app/src/main/java/de/ba/railroad/simpleclient/SimpleClientActivity.kt
+++ app/src/main/java/de/ba/railroad/simpleclient/SimpleClientActivity.kt
@@ -1,17 +1,26 @@
 package de.ba.railroad.simpleclient
 
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.relay.compose.BoxScopeInstanceImpl.align
 import com.google.relay.compose.RelayBox
@@ -64,22 +73,33 @@ class SimpleClientActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                RelayContainer {
-                    RelayBox(modifier = Modifier.fillMaxSize()){
-                        SpeedControl(
-                                onFastForwardClick = {},
-                                onForwardButtonClick = {},
-                                onStopButtonClick = {},
-                                onBackButtonClick = {},
-                                onFastBackButtonClick = {},
-                                modifier = Modifier.align(Alignment.BottomCenter)
-
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.Bottom
+                    ) {
+                        Column (
+                            modifier = Modifier.weight(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Bottom
                         )
+                        {
+                            RelayContainer {
+                                toolBar()
+                            }
+                        }
                     }
-
                 }
             }
         }
+    }
+    @Preview
+    @Composable
+    fun toolBar() {
+        SpeedControl()
     }
 
 }
