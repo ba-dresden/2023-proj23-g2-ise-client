aef7bf35dc7e02cbba48cb64a50a647e1dc038ba
justusmix <Justus.mix04@gmail.com>
2023-11-03 14:25:35 +0100

    Locomotive with rounded corners and variant

--- app/src/main/java/de/ba/railroad/simpleclient/SimpleClientActivity.kt
+++ app/src/main/java/de/ba/railroad/simpleclient/SimpleClientActivity.kt
@@ -1,38 +1,41 @@
 package de.ba.railroad.simpleclient
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.FilledTonalButton
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.DefaultTranslationX
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import com.google.relay.compose.BoxScopeInstanceImpl.align
-import com.google.relay.compose.RelayBox
-import com.google.relay.compose.RelayColumn
 import com.google.relay.compose.RelayContainer
+import com.google.relay.compose.RelayContainerScope
+import com.google.relay.compose.RelayImage
+import de.ba.railroad.simpleclient.buttonlocomotiverenate.ButtonLocomotiveRenate
+import de.ba.railroad.simpleclient.buttonlocomotiverenate.Default
 import de.ba.railroad.simpleclient.speedcontrol.SpeedControl
 import de.ba.railroadclient.CraneWebSocketClient
 import de.ba.railroadclient.LocomotiveWebSocketClient
@@ -94,9 +97,22 @@ class SimpleClientActivity : ComponentActivity() {
                             verticalArrangement = Arrangement.Bottom
                         )
                         {
+                                Card (
+                                        shape = RoundedCornerShape(30.dp),
+                                        border = BorderStroke(width = 2.dp, color = Color.White),
+                                        modifier = Modifier
+                                                .clip(RoundedCornerShape(30.dp))
+                                                .size(width = 93.dp, height = 40.dp)
+                                ){
+                                    var enabled = remember{mutableStateOf(true)}
+                                    var property = if(enabled.value) Default.Default else Default.Pressed
+                                    ButtonLocomotiveRenate(text = "Renate",
+                                            default = property,
+                                            onClick = {
+                                                enabled.value = !(enabled.value)
+                                            })
+                                }
 
-                                SpeedControl()
-                                val checkedState = remember{ mutableStateOf(false) }
                         }
                     }
                 }
